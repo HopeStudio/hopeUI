d52d0840ad719df393f65bd256bd10d6a73d5ddf
demonly <ly18824110284@gmail.com>
2018-06-01 15:46:27 +0800

    重写卡片组件

--- main/app/card/cardItem.js
+++ main/app/card/cardItem.js
@@ -1,102 +1,79 @@
 import React from 'react';
 import classnames from 'classnames';
 import PropTypes from 'prop-types';
-import $ from 'jQuery';
 
-import colors from '../rules/colors.js';
 import cssCard from './card.less';
-import uuid from '../tools/uuid.js';
-import colorTrans from '../tools/colorTrans.js';
 
 import { Browse, Comments } from '../icons/export';
 
-class CardItem extends React.Component {
-  setStyle() {
-    const { cover, avatar } = this.props;
+function CardItem(props) {
+  const {
+    uuid,
+    cover,
+    avatar,
+    title,
+    description,
+    author,
+    hits,
+    comments,
+  } = props;
 
-    colorTrans(
-      { 'background-image': `url("${cover}")` },
-      `.card-${this.uuid} .${cssCard.cover}`
-    );
-
-    colorTrans(
-      { 'background-image': `url("${avatar}")` },
-      `.card-${this.uuid} .${cssCard.avatar}`
-    );
-  }
-
-  componentWillMount() {
-    this.uuid = this.props.uuid;
-    this.setStyle();
-  }
-
-  render() {
-    const {
-      title,
-      coverBorder,
-      description,
-      author,
-      hits,
-      comments,
-    } = this.props;
-
-    return (
-      <div className={classnames(cssCard.card, `card-${this.uuid}`)}>
-        <div className={classnames(cssCard.cardContent)}>
-          <div className={classnames(cssCard.coverContainer, { coverBorder: coverBorder })}>
-            <div className={classnames(cssCard.cover)}/>
-            <div className={classnames(cssCard.info)}>
-              <div className={classnames(cssCard.title)}>{title}</div>
-              <div className={classnames(cssCard.description)}>{description}</div>
+  return (
+    <div className={classnames(cssCard.card, `card-${uuid}`)}>
+      <div className={cssCard.cardContent}>
+        <div className={cssCard.coverContainer}>
+          <div
+            className={cssCard.cover}
+            style={{ backgroundImage: `url("${cover}")` }}
+          />
+        </div>
+        <div className={cssCard.info}>
+          <div className={cssCard.title}>{title}</div>
+          <div className={cssCard.description}>{description}</div>
+        </div>
+        <div className={cssCard.item}>
+          <div className={cssCard.author}>
+            <div
+              className={cssCard.avatar}
+              style={{ backgroundImage: `url(${avatar})` }}
+            />
+            <div className={cssCard.authorName}>{author}</div>
+          </div>
+          <div className={cssCard.record}>
+            <div className={cssCard.hits}>
+              <Browse />
+              {hits}
             </div>
-            <div className={classnames(cssCard.item)}>
-              <div className={classnames(cssCard.author)}>
-                <div className={cssCard.avatar}/>
-                <div className={cssCard.authorName}>{author}</div>
-              </div>
-              <div className={classnames(cssCard.record)}>
-                <div className={classnames(cssCard.hits)}>
-                  <Browse/>
-                  {hits}
-                </div>
-                <div className={classnames(cssCard.comments)}>
-                  <Comments/>
-                  {comments}
-                </div>
-              </div>
+            <div className={cssCard.comments}>
+              <Comments />
+              {comments}
             </div>
           </div>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 CardItem.propTypes = {
+  uuid: PropTypes.string.isRequired,
   // 标题
   title: PropTypes.string.isRequired,
-
   // 描述
   description: PropTypes.string,
-
   // 封面地址
   cover: PropTypes.string,
-
   // 作者头像地址
   avatar: PropTypes.string,
-
   // 作者名称
   author: PropTypes.string,
-
   // 点击数
   hits: PropTypes.number,
-
   // 评论数
   comments: PropTypes.number,
 };
 
 CardItem.defaultProps = {
-  title: '崔霄是真的帅',
   description: '',
   cover: './dist/card/cover.jpg',
   avatar: './dist/card/avatar.png',
