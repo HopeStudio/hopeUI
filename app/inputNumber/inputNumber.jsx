1e4e6c4739c9e9eb466f9735ca9857ab637a0c26
everbrez <everbrez@gmail.com>
2018-09-21 21:16:49 +0800

    remove calculate func.

--- app/inputNumber/inputNumber.jsx
+++ app/inputNumber/inputNumber.jsx
@@ -1,106 +1,96 @@
 import { Component } from 'react';
-import cssInputNumber from './InputNumber.less';
 import classnames from 'classnames';
+import css from './InputNumber.less';
 import utils from './utils';
-
+/* eslint-disable */
 class InputNumber extends Component {
-    static defaultProps = {
-        value: 0,
-        step: 1,
-        max: 100,
-        min: -100,
-        calculable: false
-    }
+	static defaultProps = {
+		value: 0,
+		step: 1,
+		max: 10,
+		min: -10,
+	}
 
-    constructor(props) {
-        super(props);
-        // initial state
-        this.state = {
-            value: this.props.value,
-        }
+	constructor(props) {
+		super(props);
 
-        // bind methods
-        let binds = ['increase', 'decrease', 'handleChange', 'handleKeyDown', 'setValue', 'plus'];
-        for (let i in binds) {
-            this[binds[i]] = this[binds[i]].bind(this);
-        }
-    }
+		this.state = {
+			value: this.props.value,
+		}
 
-    render() {
-        const { value } = this.state;
-        return (
-            <div className={classnames(cssInputNumber.container)}>
-                <span
-                    className={classnames(cssInputNumber.btnLeft, cssInputNumber.btn)}
-                    onClick={this.decrease}>
-                </span>
-                <div className={classnames(cssInputNumber.inputContainer)}>
-                    <input
-                        type="text"
-                        className={classnames(cssInputNumber.input)}
-                        value={value}
-                        onChange={this.handleChange}
-                        onKeyDown={this.handleKeyDown}
-                        key={"244"} />
-                </div>
-                <span
-                    className={classnames(cssInputNumber.btnRight, cssInputNumber.btn)}
-                    onClick={this.increase}>
-                </span>
-            </div>
-        );
-    }
+		let binds = ['increase', 'decrease', 'handleChange', 'setValue', 'plus'];
+		for (let i in binds) {
+			this[binds[i]] = this[binds[i]].bind(this);
+		}
+	}
 
-    increase() {
-        this.plus();
-    }
+	render() {
+		const { value } = this.state;
+		const {container, btn, btnLeft, inputContainer, input, btnRight} = css;
+		return (
+			<div className={container}>
+				<span
+					className={classnames(btn, btnLeft)}
+					onClick={this.decrease}>
+				</span>
+				<div className={inputContainer}>
+					<input
+						type="text"
+						className={input}
+						value={value}
+						onChange={this.handleChange}
+						onKeyDown={this.handleKeyDown}
+						key={"244"} />
+				</div>
+				<span
+					className={classnames(btn, btnRight)}
+					onClick={this.increase}>
+				</span>
+			</div>
+		);
+	}
 
-    decrease() {
-        this.plus(-this.props.step);
-    }
+	increase() {
+		this.plus();
+	}
 
-    plus(step = this.props.step, reverse = false) {
-        this.setState((prev, props) => {
-            const { value: prevVal } = prev;
-            const { min, max } = props;
-            const curVal = utils.calculateFromString(prevVal) + (reverse ? -step : step);
-            const value = utils.range(curVal, min, max);
+	decrease() {
+		this.plus(-this.props.step);
+	}
 
-            return {
-                value
-            }
-        })
-    }
+	plus(step = this.props.step, reverse = false) {
+		this.setState((prev, props) => {
+			const { value: prevVal } = prev;
+			const { min, max } = props;
+			const curVal = prevVal + (reverse ? -step : step);
+			const value = utils.range(curVal, min, max);
 
-    handleChange(event) {
-        const { value: prevVal } = event.target;
-        let value = '';
+			return {
+				value
+			}
+		})
+	}
 
-        if (utils.isEquation(prevVal)) {
-            value = prevVal;
-            this.setValue(value);
-        }
-    }
+	handleChange(event) {
+		const { value: prevVal } = event.target;
+		let value = '';
+
+		if (utils.isEquation(prevVal)) {
+			value = prevVal;
+			this.setValue(value);
+		}
+	}
 
 
-    handleKeyDown(event) {
-        // key Enter
-        if (event.keyCode === 13) {
-            const { value } = this.state;
-            let newValue = utils.calculateFromString(value);
-            this.setValue(newValue, true);
-        }
-    }
-
-    setValue(value, range = false) {
-        if (range) {
-            const { max, min } = this.props;
-            value = utils.range(value, min, max);
-        }
-        this.setState({
-            value
-        });
-    }
+	setValue(value, range = false) {
+		if (range) {
+			const { max, min } = this.props;
+			value = utils.range(value, min, max);
+		}
+		this.setState({
+			value
+		});
+	}
 }
 
 export default InputNumber;
--- app/inputNumber/utils.js
+++ app/inputNumber/utils.js
@@ -1,37 +1,5 @@
+/* eslint-disable */
 class Utils {
-    constructor() {
-        this.calculateFromString = this.calculateFromString.bind(this);
-    }
-
-    calculateFromString(str) {
-        //if str is Number, then return str
-        if (typeof str === typeof 1) {
-            return str;
-        }
-        // if the string is not the equation format, then throw error
-        if (typeof str !== typeof '' || !this.isEquation(str)) {
-            throw new Error('unexpected string');
-        }
-        // calculate the string, now only support plus(+) and minus(-) operator
-        //initial the stack
-        const res = [''];
-
-        //process str, if the last character is '-' or '+', then del
-        if (str[str.length - 1] === '-' || str[str.length - 1] === '+') {
-            str += '0';
-        }
-        // push the number(string) and operator to the stack
-        for (let i = 0; i < str.length; i++) {
-            if (str[i] === '-' || str[i] === '+') {
-                res.push(str[i]);
-            } else {
-                let prevValue = res.pop();
-                res.push(prevValue + str[i]);
-            }
-        }
-        return res.reduce((prev, cur) => prev + +cur, 0);
-        // console.log(res);
-    }
 
     range(value, min = -Infinity, max = Infinity) {
         return value > max ? max : (value < min ? min : value);
