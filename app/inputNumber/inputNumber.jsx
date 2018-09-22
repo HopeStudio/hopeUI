138b07fc338af0e579490b9f754145537f3f4a95
everbrez <everbrez@gmail.com>
2018-09-22 16:50:18 +0800

    add [prop]precision, step--> state

--- app/inputNumber/inputNumber.jsx
+++ app/inputNumber/inputNumber.jsx
@@ -7,26 +7,30 @@ import { ArrowDown, ArrowUp } from '../icons/export';
 class InputNumber extends Component {
 	static defaultProps = {
 		value: 0,
-		step: 0.5,
+		step: 0.6,
 		max: 100,
 		min: 0,
 		disabled: false,
-		precision: true
+		precision: false
 	}
 
 	constructor(props) {
 		super(props);
-
+		const { max, min, step, precision, value } = Object.assign({}, this.defaultProps, props);
 		this.state = {
-			value: this.props.value,
-			btnLeftDisable: false,
-			btnRightDisable: false
+			max: this.format(max, precision),
+			min: this.format(min, precision),
+			step: this.format(step, precision),
+			value: this.format(value, precision)
 		}
-
-		let binds = ['increase', 'decrease', 'handleChange', 'setValue', 'plus','checkValue','test'];
+		let binds = ['increase', 'decrease', 'handleChange', 'setValue', 'plus', 'checkValue', 'test', 'handleBlur', 'handleEnterDown'];
 		for (let i in binds) {
 			this[binds[i]] = this[binds[i]].bind(this);
 		}
+	}
+
+	format(props, precision) {
+		return precision ? +props.toFixed(precision) : +props.toFixed(0);
 	}
 
 	componentDidMount() {
@@ -34,31 +38,36 @@ class InputNumber extends Component {
 	}
 
 	render() {
-		const {disabled: inputDisable} = this.props;
+		const { disabled: inputDisable } = this.props;
 		const { value, btnLeftDisable, btnRightDisable } = this.state;
-		const {container, btn, btnLeft, inputContainer, input, btnRight, disable} = css;
+		const { container, btn, btnLeft, inputContainer, input, btnRight, disable } = css;
 		return (
-			<div className={classnames(container,{[disable]: inputDisable})}>
-			<Button onClick={this.decrease} 
-			className={classnames(btn, btnLeft)} 
-			icon={<ArrowDown width='100%' height='100%'/>}
-			type={btnLeftDisable || inputDisable ? 'disabled': 'activate'}
-			content=''/>
+			<div className={classnames(container, { [disable]: inputDisable })}>
+				<Button onClick={this.decrease}
+					className={classnames(btn, btnLeft)}
+					icon={<ArrowDown width='100%' height='100%' />}
+					type={btnLeftDisable || inputDisable ? 'disabled' : 'activate'}
+					content=''
+				/>
 				<div className={inputContainer}>
 					<input
 						type="text"
 						className={input}
 						value={value}
 						onChange={this.handleChange}
-						key={"244"} 
-						disabled={inputDisable}/>
+						key={"244"}
+						disabled={inputDisable}
+						onBlur={this.handleBlur}
+						onKeyDown={this.handleEnterDown}
+					/>
 				</div>
-			<Button 
-			onClick={this.increase} 
-			className={classnames(btn, btnRight)}
-			icon={<ArrowUp width='100%' height='100%'/>} 
-			type={btnRightDisable || inputDisable ? 'disabled': 'activate'}
-			content=''/>
+				<Button
+					onClick={this.increase}
+					className={classnames(btn, btnRight)}
+					icon={<ArrowUp width='100%' height='100%' />}
+					type={btnRightDisable || inputDisable ? 'disabled' : 'activate'}
+					content=''
+				/>
 			</div>
 		);
 	}
@@ -68,50 +77,81 @@ class InputNumber extends Component {
 	}
 
 	decrease() {
-		this.plus(-this.props.step);
+		this.plus(-this.state.step);
 	}
 
-	plus(step = this.props.step) {
-		if(this.props.disabled) {
+	plus(step = this.state.step) {
+		if (this.props.disabled) {
 			return;
 		}
-		this.setValue(preValue => preValue + step);
+		this.setValue(preValue => +preValue + step);
 	}
 
 	handleChange(event) {
 		const { value } = event.target;
-		if(this.test(value)) {
-			this.setValue(+value);
+		console.log(this.test(value));
+		if (this.test(value)) {
+			this.setValue(value);
 		}
 	}
 
 	setValue(value = this.props.value) {
 		this.setState(prev => {
-			return this.checkValue(typeof value === typeof (()=>{}) ? value(prev.value) : value);
+			return this.checkValue(typeof value === typeof (() => { }) ? value(prev.value) : value);
 		});
 	}
 
 	checkValue(value) {
-		const { max, min } = this.props;
-		let btnLeftDisable = false;
-		let btnRightDisable = false;
-		if(value >= max) {
-			btnRightDisable = true;
-			value = max;
-		} else if (value <= min) {
-			btnLeftDisable = true;
-			value = min;
+		const { max, min, precision } = this.props;
+		value = +value > max
+			? max : +value < min
+				? min : value;
+		let btnState = this.checkButton(+value);
+
+		if (precision) {
+			value = typeof value === typeof 1
+				? value.toFixed(precision) : value;
 		}
+
 
 		return {
 			value,
-			btnRightDisable,
-			btnLeftDisable
+			...btnState
 		};
 	}
 
+	checkButton(value) {
+		let btnLeftDisable = false;
+		let btnRightDisable = false;
+		const { max, min } = this.props;
+
+		value >= max
+			? btnRightDisable = true : value <= min
+				? btnLeftDisable = true : 0;
+
+		return {
+			btnLeftDisable,
+			btnRightDisable
+		}
+	}
+
 	test(value) {
-		return /^-?\d+$/g.test(value);
+		if (this.props.precision) {
+			return /(^-?\d+\.?\d*$)|(^[\s\S]{0}$)/g.exec(value);
+		}
+		return /^-?\d+$|^[\s\S]{0}$/g.exec(value);
+	}
+
+	handleBlur(event) {
+		const { value } = event.target;
+		this.setValue(+value);
+	}
+
+	handleEnterDown(event) {
+		if (event.keyCode === 13) {
+			const { value } = event.target;
+			this.setValue(+value);
+		}
 	}
 
 }
